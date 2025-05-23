b2db1ec221f1c8ac2a6365480568a257efdcebc8
Mdln0 <soaremadalincosmin@gmail.com>
2025-05-23 08:08:19 +0300

    Update papaparse.d.ts

--- papaparse.d.ts
+++ papaparse.d.ts
@@ -1,10 +1,4 @@
 declare module "papaparse" {
-  export function parse(
-    input: string | File,
-    config?: any
-  ): {
-    data: any[];
-    errors: any[];
-    meta: any;
-  };
+  const Papa: any;
+  export = Papa;
 }
